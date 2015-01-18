e8f8897662c857e8725effeec59bf5b8379412ac
Danila <grohotun@gmail.com>
2015-01-18 16:01:54 +0400

    v 1.0.1

--- app/code/local/Mygento/Base/Block/Adminhtml/Info.php
+++ app/code/local/Mygento/Base/Block/Adminhtml/Info.php
@@ -29,9 +29,9 @@ class Mygento_Base_Block_Adminhtml_Info extends Mage_Adminhtml_Block_Abstract im
                 .'.line{border-top: 1px solid #c6c6c6;padding-top:15px;}'
                 .'}</style>';
         $html.='<table id="mygento_info" cellspacing="0" cellpading="0">';
-        $html.='<tr><td>'.$helper->__('Support:').'</td><td>'.$helper->__('Paid extensions support is available through <a href="%s" target="_blank">ticket tracking system</a>',$ticket_url).'.<br/><br/>'.$helper->__('Please report is all bugs and feature requests.').'<br/><br/>'.$helper->__('If for some reasons you can not submit ticket to our system, you can write us an email %s.',$email).'</td></tr>';
-        $html.='<tr><td>'.$helper->__('License:').'</td><td>'.$helper->__('You can check tender offer <a href="http://www.mygento.ru/oferta" target="_blank">here</a>').'</td></tr>';
-        $html.='<tr class="line"><td><img src="//www.mygento.ru/media/wysiwyg/logo_base.png" width="100" height="133"/></td><td>'.$helper->__('You can hire us for any Magento extension modification.<br/>Write us to %s',$email).'<br/><br/>'.$helper->__('You can check all providable services on <a href="%s" target="_blank">our website</a>.',$site.'/services').'</td></tr>';
+        $html.='<tr><td>'.$helper->__('Support:').'</td><td>'.$helper->__('Purchased extensions support is available through <a href="%s" target="_blank">ticket tracking system</a>',$ticket_url).'.<br/><br/>'.$helper->__('Please report all bugs and feature requests.').'<br/><br/>'.$helper->__('If for some reasons you can not submit ticket to our system, you can write us an email %s.',$email).'</td></tr>';
+        $html.='<tr><td>'.$helper->__('License:').'</td><td>'.$helper->__('Tender offer can be checked <a href="http://www.mygento.ru/oferta" target="_blank">here</a>').'</td></tr>';
+        $html.='<tr class="line"><td><img src="//www.mygento.ru/media/wysiwyg/logo_base.png" width="100" height="133"/></td><td>'.$helper->__('You can hire us for any Magento extension customization and development.<br/>Write us to %s',$email).'<br/><br/>'.$helper->__('You can check all providable services on <a href="%s" target="_blank">our website</a>.',$site.'/services').'</td></tr>';
         $html.= '</table>';
 
         return $html;
