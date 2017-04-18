ad723ed5f60993943f2e39a04ae0169b3dc94128
Danila Tarasov <grohotun@gmail.com>
2017-04-18 16:32:45 +0300

    move list model to m2

---
+++ Model/Source/Attributes.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @author Mygento
+ * @copyright See COPYING.txt for license details.
+ * @package Mygento_Base
+ */
+
+namespace Mygento\Base\Model\Source;
+
+class Attributes implements \Magento\Framework\Option\ArrayInterface
+{
+
+    /**
+     * Possible payment types
+     *
+     * @return array
+     */
+    public function getAllOptions()
+    {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $coll = $objectManager->create(\Magento\Catalog\Model\ResourceModel\Product\Attribute\Collection::class);
+
+        $coll->addFieldToFilter(\Magento\Eav\Model\Entity\Attribute\Set::KEY_ENTITY_TYPE_ID, 4);
+
+        $coll->addFieldToFilter('main_table.frontend_input', ['neq' => 'hidden']);
+        $coll->addFieldToFilter('main_table.frontend_input', ['neq' => 'multiselect']);
+        $coll->addFieldToFilter('main_table.frontend_input', ['neq' => 'boolean']);
+        $coll->addFieldToFilter('main_table.frontend_input', ['neq' => 'date']);
+        $coll->addFieldToFilter('main_table.frontend_input', ['neq' => 'image']);
+        $coll->addFieldToFilter('main_table.frontend_input', ['neq' => 'price']);
+        $coll->addFieldToFilter('used_in_product_listing', '1');
+        $coll->setOrder('frontend_label', 'ASC');
+
+
+        $attrAll = $coll->load()->getItems();
+
+        $_options = [];
+
+        $_options[] = [
+            'label' => __('No usage'),
+            'value' => 0
+        ];
+
+        // Loop over all attributes
+        foreach ($attrAll as $attr) {
+
+            $label = $attr->getStoreLabel() ? $attr->getStoreLabel() : $attr->getFrontendLabel();
+            if ('' != $label) {
+                $_options[] = ['label' => $label, 'value' => $attr->getAttributeCode()];
+            }
+        }
+        return $_options;
+    }
+
+    public function toOptionArray()
+    {
+        return $this->getAllOptions();
+    }
+
+}
