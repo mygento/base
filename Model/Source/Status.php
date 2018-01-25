861b5ea631dcb8c98b1a07c8cb5b7674452f3d95
nikita tarasov <nikita@mygento.ru>
2018-01-25 18:12:27 +0300

    bump 2.2.8
    Order Status source model

---
+++ Model/Source/Status.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author Mygento
+ * @copyright See COPYING.txt for license details.
+ * @package Mygento_Base
+ */
+
+namespace Mygento\Base\Model\Source;
+
+class Status implements \Magento\Framework\Option\ArrayInterface
+{
+    const UNDEFINED_OPTION_LABEL = '-- Please Select --';
+
+    /**
+     * @var \Magento\Sales\Model\Order\Config
+     */
+    protected $_orderConfig;
+    /**
+     * @param \Magento\Sales\Model\Order\Config $orderConfig
+     */
+    public function __construct(\Magento\Sales\Model\Order\Config $orderConfig)
+    {
+        $this->_orderConfig = $orderConfig;
+    }
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $statuses = $this->_orderConfig->getStatuses();
+        $options = [['value' => '', 'label' => __('-- Please Select --')]];
+        foreach ($statuses as $code => $label) {
+            $options[] = ['value' => $code, 'label' => $label];
+        }
+        return $options;
+    }
+}
