d5ba066b2a536b7b9fb9347b3b17b0069e23833b
Kirill Kolchin <k.kolchin@mygento.ru>
2018-05-15 13:07:31 +0300

    Add currency helper

---
+++ Helper/Currency.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @author Mygento
+ * @copyright See COPYING.txt for license details.
+ * @package Mygento_Base
+ */
+
+namespace Mygento\Base\Helper;
+
+use Magento\Directory\Model\CurrencyFactory;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Base Data helper
+ */
+class Currency extends AbstractHelper
+{
+    /**
+     * @var array
+     */
+    private $currencyCodes = [
+        'RU' => 'RUB',
+        'BY' => 'BYR',
+        'FR' => 'EUR',
+        'GB' => 'GBP',
+        'IT' => 'EUR',
+        'UA' => 'UAH',
+        'KZ' => 'KZT',
+    ];
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var CurrencyFactory
+     */
+    private $currencyFactory;
+
+    /**
+     * Currency constructor.
+     * @param Context $context
+     * @param StoreManagerInterface $storeManager
+     * @param CurrencyFactory $currencyFactory
+     */
+    public function __construct(
+        Context $context,
+        StoreManagerInterface $storeManager,
+        CurrencyFactory $currencyFactory
+    ) {
+        parent::__construct($context);
+        $this->storeManager = $storeManager;
+        $this->currencyFactory = $currencyFactory;
+    }
+
+    /**
+     * @param $amountValue
+     * @param null $currencyCodeFrom
+     * @param null $currencyCodeTo
+     * @return float|int
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function convert($amountValue, $currencyCodeFrom = null, $currencyCodeTo = null)
+    {
+        /**
+         * If is not specified the currency code from which we want to convert
+         * - use current currency
+         */
+        if (!$currencyCodeFrom) {
+            $currencyCodeFrom = $this->storeManager->getStore()
+                ->getCurrentCurrency()->getCode();
+        }
+
+        /**
+         * If is not specified the currency code to which we want to convert
+         * - use base currency
+         */
+        if (!$currencyCodeTo) {
+            $currencyCodeTo = $this->storeManager->getStore()
+                ->getBaseCurrency()->getCode();
+        }
+
+        /**
+         * Do not convert if currency is same
+         */
+        if ($currencyCodeFrom == $currencyCodeTo) {
+            return $amountValue;
+        }
+
+        /** @var float $rate */
+        // Get rate
+        $rate = $this->currencyFactory->create()
+            ->load($currencyCodeFrom)->getAnyRate($currencyCodeTo);
+
+        if (!$rate) {
+            throw new \Exception(__('Cannot find currency rate'));
+        }
+
+        // Get amount in new currency
+        $amountValue = $amountValue * $rate;
+
+        return $amountValue;
+    }
+
+    /**
+     * @param $iso
+     * @return mixed
+     */
+    public function getCodeByCountryIso($iso)
+    {
+        if (key_exists($iso, $this->currencyCodes)) {
+            return $this->currencyCodes[$iso];
+        }
+        // get first value as default
+        return reset($this->currencyCodes);
+    }
+}
